d8782174b9570da031e8e13cfe9b696099313f9f
DioBrando0203 <147171505+DioBrando0203@users.noreply.github.com>
2025-11-03 22:00:22 +0000

    internacional

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Estados Unidos y China reanudan conversaciones económicas de alto nivel en Pekín",
+  "autor" => "Demócrata",
+  "resumen" => "Delegaciones de alto nivel de Estados Unidos y China se reunieron en Pekín para retomar el diálogo económico bilateral, tras meses de tensiones comerciales y tecnológicas. Ambas potencias coincidieron en la necesidad de mantener la comunicación abierta y evitar una escalada que afecte al comercio global.",
+  "imagen" => "/workspaces/noticiero-armas/internacional.jpg"
 ];
 ?>
