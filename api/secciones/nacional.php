d40409dc0b0b3ba675ec7a3926e58513be1dd097
DioBrando0203 <147171505+DioBrando0203@users.noreply.github.com>
2025-11-03 21:56:38 +0000

    nacional

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,11 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Dina Boluarte deja la presidencia del Perú tras semanas de crisis política",
+"autor" => "La República",
+"resumen" => "Dina Boluarte deja la presidencia como la mandataria más impopular del mundo y Perú: 93% de desaprobación y 0% entre jóvenes y el sur
+",
+"imagen" => "/workspaces/noticiero-armas/dina.png"
+
 ];
 ?>
